745152c83ac897e57eaede4320f7452b524fc726
Jonas Balsfulland <jonas@balsfulland.net>
2015-04-29 13:10:09 +0200

    Fixed TimeUtil

--- src/main/java/net/spleefleague/core/utils/TimeUtil.java
+++ src/main/java/net/spleefleague/core/utils/TimeUtil.java
@@ -6,7 +6,6 @@
 package net.spleefleague.core.utils;
 
 import java.time.Duration;
-import java.time.Period;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,31 +14,46 @@ import java.util.regex.Pattern;
  * @author Jonas
  */
 public class TimeUtil {
-    
+
     public static String getFormatted(Duration d) {
         String s = "";
         boolean started = false;
-        if(started || d.toDays() > 0) {
+        if (started || d.toDays() >= 365) {
+            started = true;
+            s += d.toDays() / 365 + " year" + ((d.toDays() / 365 != 1) ? "s" : "") + ", ";
+            d = d.minus(Duration.ofDays(d.toDays() / 365 * 365));
+        }
+        if (started || d.toDays() >= 30) {
+            started = true;
+            s += d.toDays() / 30 + " month" + ((d.toDays() / 30 != 1) ? "s" : "") + ", ";
+            d = d.minus(Duration.ofDays(d.toDays() / 30 * 30));
+        }
+        if (started || d.toDays() >= 7) {
+            started = true;
+            s += d.toDays() / 7 + " week" + ((d.toDays() / 7 != 1) ? "s" : "") + ", ";
+            d = d.minus(Duration.ofDays(d.toDays() / 7 * 7));
+        }
+        if (started || d.toDays() > 0) {
             started = true;
             s += d.toDays() + " day" + ((d.toDays() != 1) ? "s" : "") + ", ";
             d = d.minus(Duration.ofDays(d.toDays()));
         }
-        if(started || d.toHours()> 0) {
+        if (started || d.toHours() > 0) {
             s += d.toHours() + " hour" + ((d.toHours() != 1) ? "s" : "") + ", ";
             started = true;
             d = d.minus(Duration.ofHours(d.toHours()));
         }
-        if(started || d.toMinutes()> 0) {
+        if (started || d.toMinutes() > 0) {
             s += d.toMinutes() + " minute" + ((d.toMinutes() != 1) ? "s" : "") + " and ";
             started = true;
             d = d.minus(Duration.ofMinutes(d.toMinutes()));
         }
-        if(started || d.getSeconds()> 0) {
+        if (started || d.getSeconds() > 0) {
             s += d.getSeconds() + " second" + ((d.getSeconds() != 1) ? "s" : "");
         }
         return s;
     }
-    
+
     public static Duration parseDurationString(String time) {
 
         Pattern timePattern = Pattern.compile("[1-9][0-9]*(y|mo|w|h|d|m|s)");
@@ -51,10 +65,10 @@ public class TimeUtil {
         int hours = 0;
         int minutes = 0;
         int seconds = 0;
-        
+
         while (matcher.find()) {
             String r = matcher.group();
-            switch(r.charAt(r.length() - 1)) {
+            switch (r.charAt(r.length() - 1)) {
                 case 'y': {
                     years = Integer.parseInt(r.replace("y", ""));
                     break;
@@ -64,7 +78,7 @@ public class TimeUtil {
                     break;
                 }
                 case 'w': {
-                    weeks = Integer.parseInt(r.replace("y", ""));
+                    weeks = Integer.parseInt(r.replace("w", ""));
                     break;
                 }
                 case 'd': {
@@ -83,9 +97,9 @@ public class TimeUtil {
                     seconds = Integer.parseInt(r.replace("s", ""));
                     break;
                 }
-                
+
             }
         }
-        return Duration.ofDays(Period.ofYears(years).plusMonths(months).plusDays(weeks * 7).plusDays(days).getDays()).plusHours(hours).plusMinutes(minutes).plusSeconds(seconds);
+        return Duration.ofDays(years * 365 + months * 30 + weeks * 7 + days).plusHours(hours).plusMinutes(minutes).plusSeconds(seconds);
     }
 }
