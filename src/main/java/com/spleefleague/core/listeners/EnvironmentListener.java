abe6c95e482d3520218019fe70f235dec2dbb120
Wouter Gerarts <wouto1997@gmail.com>
2016-03-23 08:32:03 +0100

    Small fix allowing servers with bare configs to work better. Ignores the non-existance of "blocked_commands" in settings

--- src/main/java/com/spleefleague/core/listeners/EnvironmentListener.java
+++ src/main/java/com/spleefleague/core/listeners/EnvironmentListener.java
@@ -204,7 +204,10 @@ public class EnvironmentListener implements Listener{
     @EventHandler
     public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
         String cmd = event.getMessage().split(" ")[0].toLowerCase();
-        if(((List<String>)Settings.getList("blocked_commands")).contains(cmd)) {
+        if (!Settings.hasKey("blocked_commands")) {
+            return;
+        }
+        if (((List<String>) Settings.getList("blocked_commands")).contains(cmd)) {
             event.setCancelled(true);
         }
     }
