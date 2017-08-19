b4e8fb05fbac285105a12a8e1c203328037f77cd
Jonas Balsfulland <jonas@balsfulland.net>
2017-08-19 17:17:31 +0200

    Allowing /ticketreply with offline connection client

--- src/main/java/com/spleefleague/core/command/commands/ticketreply.java
+++ src/main/java/com/spleefleague/core/command/commands/ticketreply.java
@@ -1,19 +1,16 @@
 package com.spleefleague.core.command.commands;
 
 import com.spleefleague.core.SpleefLeague;
-import com.spleefleague.core.chat.ChatChannel;
-import com.spleefleague.core.chat.ChatManager;
 import com.spleefleague.core.command.BasicCommand;
+import com.spleefleague.core.io.Config;
+import com.spleefleague.core.io.connections.ConnectionClient;
 import com.spleefleague.core.io.connections.ConnectionResponseHandler;
 import com.spleefleague.core.player.Rank;
 import com.spleefleague.core.player.SLPlayer;
 import com.spleefleague.core.plugin.CorePlugin;
 import com.spleefleague.core.utils.StringUtil;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
-import net.md_5.bungee.api.chat.HoverEvent;
+import com.sun.scenario.Settings;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.entity.Player;
 import org.json.simple.JSONObject;
@@ -31,36 +28,48 @@ public class ticketreply extends BasicCommand {
     @Override
     protected void run(Player p, SLPlayer slp, Command cmd, String[] args) {
         if (args.length > 1) {
-            Bukkit.getScheduler().runTaskAsynchronously(SpleefLeague.getInstance(), () -> {
-                SLPlayer slPlayer = SpleefLeague.getInstance().getPlayerManager().loadFake(args[0]);
-                if(slPlayer == null) {
-                    error(p, args[0] + " has never played on SpleefLeague!");
+            ConnectionClient cc = SpleefLeague.getInstance().getConnectionClient();
+            String message = StringUtil.fromArgsArray(args, 1);
+            if(cc.isEnabled()) {
+                Bukkit.getScheduler().runTaskAsynchronously(SpleefLeague.getInstance(), () -> {
+                    SLPlayer target = SpleefLeague.getInstance().getPlayerManager().loadFake(args[0]);
+                    if(target == null) {
+                        error(p, args[0] + " has never played on SpleefLeague!");
+                        return;
+                    }
+                    JSONObject request = new JSONObject();
+                    request.put("uuid", target.getUniqueId().toString());
+                    request.put("action", "GET_PLAYER");
+                    new ConnectionResponseHandler("sessions", request, 40) {
+
+                        @Override
+                        protected void response(JSONObject jsonObject) {
+                            if(jsonObject == null || jsonObject.get("playerServer").toString().equalsIgnoreCase("OFFLINE")) {
+                                error(slp, target.getName() + " isn't online!");
+                                return;
+                            }
+                            JSONObject sendObject = new JSONObject();
+                            sendObject.put("rankColor", slp.getRank().getColor().name());
+                            sendObject.put("sendUUID", target.getUniqueId());
+                            sendObject.put("sendName", target.getName());
+                            sendObject.put("shownName", slp.getName());
+                            sendObject.put("overrideServer", jsonObject.get("playerServer").toString());
+                            sendObject.put("message", message);
+                            SpleefLeague.getInstance().getConnectionClient().send("ticket", sendObject);
+                        }
+
+                    };
+                });
+            }
+            else {
+                SLPlayer target = SpleefLeague.getInstance().getPlayerManager().get(args[0]);
+                if(target == null) {
+                    error(slp, args[0] + " isn't online!");
                     return;
                 }
-                JSONObject request = new JSONObject();
-                request.put("uuid", slPlayer.getUniqueId().toString());
-                request.put("action", "GET_PLAYER");
-                new ConnectionResponseHandler("sessions", request, 40) {
-
-                    @Override
-                    protected void response(JSONObject jsonObject) {
-                        if(jsonObject == null || jsonObject.get("playerServer").toString().equalsIgnoreCase("OFFLINE")) {
-                            error(slp, slPlayer.getName() + " isn't online!");
-                            return;
-                        }
-                        String message = StringUtil.fromArgsArray(args, 1);
-                        JSONObject sendObject = new JSONObject();
-                        sendObject.put("rankColor", slp.getRank().getColor().name());
-                        sendObject.put("sendUUID", slPlayer.getUniqueId());
-                        sendObject.put("sendName", slPlayer.getName());
-                        sendObject.put("shownName", slp.getName());
-                        sendObject.put("overrideServer", jsonObject.get("playerServer").toString());
-                        sendObject.put("message", message);
-                        SpleefLeague.getInstance().getConnectionClient().send("ticket", sendObject);
-                    }
-
-                };
-            });
+                ticket ticketCommand = (ticket)SpleefLeague.getInstance().getBasicCommand("ticket");
+                ticketCommand.sendTicket(target.getName(), slp.getName(), target.getUniqueId(), message, Config.getString("server_name"), slp.getRank().getColor());
+            }
         } else {
             sendUsage(p);
         }
